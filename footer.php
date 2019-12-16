6b597f79dba4e342cf41b61f685ef94b8a076c35
Your Name <you@example.com>
2019-12-16 21:10:05 +0700

    add lien he va gioi thieu

---
+++ footer.php
@@ -0,0 +1,48 @@
+<footer>
+  <div class="infor" style="background-color: #96bceb;">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-12 col-sm-12">
+          <div class="maps">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.6318949112747!2d105.82253501484145!3d21.00738779388369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac8109765ba5%3A0xd84740ece05680ee!2zVHLGsOG7nW5nIMSQ4bqhaSBI4buNYyBUaOG7p3kgTOG7o2k!5e0!3m2!1svi!2s!4v1576428139179!5m2!1svi!2s"
+              width="300" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-12 col-sm-12">
+          <p>
+            TRƯỜNG ĐẠI HỌC THỦY LỢI <br>
+            Địa chỉ :175 Tây Sơn, Đống Đa, Hà Nội <br>
+            Điện thoại:(024)38522201 -Fax: (024)35633351 <br>
+            Email:phonghcth@tlu.edu.vn
+          </p>
+          <p>
+            TRƯỜNG ĐẠI HỌC THỦY LỢI - CƠ SỞ 2 <br>
+            Phường An Thạnh - TX Thuận An - Tỉnh Bình Dương <br>
+            Điện thoại: (84).650.3748 620 <br>
+            Fax:(84).650.3833 489 <br>
+          </p>
+
+        </div>
+        <div class="col-lg-4 col-md-12 col-sm-12">
+          <p>
+            TRƯỜNG ĐẠI HỌC THỦY LỢI - CƠ SỞ 2
+            Địa chỉ : Số 2 - Đường Trường Sa - P.17 - Q.Bình Thạnh - Tp.Hồ Chí Minh
+            Điện thoại: (84)028.38 400 532 - Fax: (84)028.38 400542
+            Email: cs2@tlu.edu.vn
+          </p>
+          <p>
+            VIỆN ĐÀO TẠO VÀ KHOA HỌC ỨNG DỤNG MIỀN TRUNG
+            Địa chỉ: Số 115 Trần Phú, Thành phố Phan Rang, Tỉnh Ninh Thuận
+            Điện thoại: 02593.823 027, 02593.823 028
+            Email: trungtamdh2@tlu.edu.vn
+          </p>
+        </div>
+      </div>
+      <div class="mess">
+        <div class="support" style="background-color: gray; color: blue; width: 200px;">Bạn cần hỗ trợ ?</div>
+        <img style="height: 40px; width: 40px;" src="img/icon-mess.png" alt="">
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ gioithieu.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Đại Học Thủy Lợi</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css.map">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <script src="js/bootstrap.js"></script>
+  <link rel="stylesheet" href="css/STYLE.CSS">
+  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap&subset=vietnamese" rel="stylesheet">
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+  <?php include('header.php') ?>
+  <main>
+    <div class="gioithieu">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h2>Giới thiệu</h2>
+            <div class="menu">
+              <ul>
+                <li><a href="#">Sứ mạng, Tầm nhìn</a></li>
+                <li>
+                  <a href="#">Tổng quan</a>
+                  <ul class="sub-menu">
+                    <li>
+                      <a href="#">Lịch sử-Sự kiện</a>
+                      <ul class="sub-menu1">
+                        <li><a href="#">Lịch sử nhà trường</a></li>
+                        <li>
+                          <a href="#">Sự kiện</a>
+                          <ul class="sub-menu2">
+                            <li><a href="#">55 năm thành lập trường</a></li>
+                            <li><a href="#">50 năm thành lập trường</a></li>
+                            <li><a href="#">Sự kiện nổi bật</a></li>
+                            <li><a href="#">35 năm các đoàn ĐH</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                    <li><a href="#">Chiến lược phát triển</a></li>
+                    <li><a href="#">Thành tích-Giải thưởng</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Cơ cấu tổi chức và nhân lực</a>
+                  <ul class="sub-menu">
+                    <li>
+                      <a href="#">Cơ cấu tổ chức</a>
+                      <ul class="sub-menu1">
+                        <li><a href="#">Đảng bộ trường</a></li>
+                        <li><a href="#">Hội đồng trường</a></li>
+                        <li><a href="#">Ban giám hiểu</a></li>
+                        <li><a href="#">Các tổ chức đoàn thể</a></li>
+                        <li>
+                          <a href="#">Các đơn vị trực thuộc</a>
+                          <ul class="sub-menu2">
+                            <li><a href="#">Các đơn vị đào tạo</a></li>
+                            <li><a href="#">Các đơn vị KHCN kết hợp đào tạo</a></li>
+                            <li><a href="#">Các đơn vị quản lý phục vụ</a></li>
+                            <li><a href="#">Cơ sở 2 ĐHTL</a></li>
+                            <li><a href="#">Cơ sở Phố Hiến</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <a href="#">Tổng quan về nhân lực</a>
+                    </li>
+                  </ul>
+                </li>
+                <li><a href="#">Cơ sở vật chất</a></li>
+                <li><a href="#">Cam kết đảm bảo chất lượng đào tạo</a></li>
+                <li>
+                  <a href="#">Phổ biến pháp luật</a>
+                  <ul class="sub-menu">
+                    <li><a href="#">Nổi dung phổ biến pháp luật</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-8">
+            <img src="img/004.jpg" alt="" style="width: 858px; height: 240px;">
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  <?php include('footer.php') ?>
+</body>
+</html>
